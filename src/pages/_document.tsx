ef3624d32bfa143d39b359413482e765650ea42d
Frédéric Bolvin <frederic@bol.vin>
2020-07-27 11:35:15 +0200

    disable tracking cookies

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,17 +11,16 @@ export default class MyDocument extends Document {
     const trackingScript =
       this.props.__NEXT_DATA__.buildId === 'development'
         ? ''
-        : `var _paq = window._paq || [];
-    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-    _paq.push(['enableLinkTracking']);
-    (function() {
-    var u="https://analytics.bol.vin/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '1']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-  })();
-  `;
+        : `var _paq = window._paq = window._paq || [];
+        _paq.push(['disableCookies']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+          var u="https://analytics.bol.vin/";
+          _paq.push(['setTrackerUrl', u+'matomo.php']);
+          _paq.push(['setSiteId', '1']);
+          var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+          g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+        })();`;
 
     return (
       <Html lang="de">
